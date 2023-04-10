c4a3b04e9040074fe0f2e6c76a33095d4c6202d7
nguyễn long <nguyenthanhlong081120022@gmail.com>
2023-04-10 16:13:28 +0700

    menu

---
+++ QuanLyBanHang/View/FrmTrangChu/FormMenuNhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang.View.FrmTrangChu
+{
+    public partial class FormMenuNhanVien : Form
+    {
+        public FormMenuNhanVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
